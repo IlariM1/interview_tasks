bb044cdc3e853dc0e9cf90d78e63d666e7718210
IlariM1 <89398939+IlariM1@users.noreply.github.com>
2025-03-16 11:40:12 +0200

    Add files via upload

---
+++ data_manipulation/updater.py
@@ -0,0 +1,19 @@
+def increase_salary():
+    """
+    Task:
+        - Read the input.json file.
+        - increase all Engineering departments salaries by 25%.
+        - add new employee called Matti Meikalainen into the HR department. He is 22 years old
+          and has 3000 euros salary.
+        - write results into new_employee_data.json file.
+
+    Use functions for:
+        reading the file,
+        increasing the salary,
+        adding the new employee
+        and writing the file
+    """
+    ...
+
+if __name__ == '__main__':
+    increase_salary()
