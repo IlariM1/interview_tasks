29258b68c6b7b1b7e89839ca0f46fe740f8864aa
IlariM1 <89398939+IlariM1@users.noreply.github.com>
2025-03-16 11:41:26 +0200

    Add files via upload

--- data_manipulation/updater.py
+++ data_manipulation/updater.py
@@ -5,12 +5,15 @@ def increase_salary():
         - increase all Engineering departments salaries by 25%.
         - add new employee called Matti Meikalainen into the HR department. He is 22 years old
           and has 3000 euros salary.
+        - You have noticed shady person in the employees that needs to be removed that does not have
+          department specified those needs to be romed from the employees.
         - write results into new_employee_data.json file.
 
     Use functions for:
         reading the file,
         increasing the salary,
         adding the new employee
+        remove employees who does not have department specified.
         and writing the file
     """
     ...
